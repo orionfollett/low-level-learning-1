1ea5d08514ae2932badce700faf00a8f5b7c3541
Orion Follett <orion.follett@firstresonance.io>
2025-10-31 14:44:44 -0700

    stack init

--- src/stack.c
+++ src/stack.c
@@ -5,3 +5,23 @@
 void hello_world(void) {
   printf("hello world\n");
 }
+
+static const int LEN_INDEX = 0;
+static const int SIZE_INDEX = 1;
+
+/* Creates a new stack initialized to size: size. */
+Stack stack(Alloc alloc, int size){
+  Stack s = (Stack) alloc(size + 2);
+  s[LEN_INDEX] = 0;
+  s[SIZE_INDEX] = (int) size;
+  return s;
+}
+
+/* Pushes a new value onto the stack, returns the modified Stack. */
+Stack push(Alloc alloc, int val);
+
+/* Pops a value off the stack. Modifies stack in place. */
+int pop(Stack s);
+
+/* Returns the latest value off the stack without popping. */
+int peek(Stack s);
--- src/stack.h
+++ src/stack.h
@@ -1,6 +1,21 @@
 #ifndef STACK_H
 #define STACK_H
 
+typedef int* Stack;
+typedef int*(*Alloc)(int);
+
 void hello_world(void);
+
+/* Creates a new stack initialized to size: size. */
+Stack stack(Alloc alloc, int size);
+
+/* Pushes a new value onto the stack, returns the modified Stack. */
+Stack push(Alloc alloc, int val);
+
+/* Pops a value off the stack. Modifies stack in place. */
+int pop(Stack s);
+
+/* Returns the latest value off the stack without popping. */
+int peek(Stack s);
 
 #endif
