9ff9bd3b6e4ff326e62d0c94f696a1df4637eab3
Orion Follett <orion.follett@firstresonance.io>
2025-10-31 16:17:42 -0700

    wip

--- src/stack.c
+++ src/stack.c
@@ -6,7 +6,7 @@ static const int SIZE_INDEX = 1;
 
 /* Creates a new stack initialized to size. */
 Stack stack(int size) {
-  Stack s = (Stack)malloc(((unsigned long)size + 2)*sizeof(int));
+  Stack s = (Stack)malloc(((unsigned long)size + 2) * sizeof(int));
   s[LEN_INDEX] = 0;
   s[SIZE_INDEX] = (int)size;
   return s;
@@ -17,21 +17,29 @@ Stack push(Stack s, int val) {
   int size = s[SIZE_INDEX];
   int len = s[LEN_INDEX];
 
-  if(len >= size) {
+  if (len >= size) {
     size = size * 2;
     s = (Stack)realloc(s, ((unsigned long)size + 2) * sizeof(int));
   }
 
-  s[len+2] = val;
+  s[len + 2] = val;
   s[LEN_INDEX]++;
   return s;
 }
 
 /* Pops a value off the stack. Modifies stack in place. */
-int pop(Stack s);
+int pop(Stack s) {
+  int len = s[LEN_INDEX];
+  s[LEN_INDEX] = len - 1;
+  return s[len];
+}
 
 /* Returns the latest value off the stack without popping. */
-int peek(Stack s);
+int peek(Stack s) {
+  return s[s[LEN_INDEX]];
+}
 
 /* Deallocate memory associated with stack. */
-void destroy_stack(Stack s);
+void destroy_stack(Stack s) {
+  free(s);
+}
--- tests/test_stack.c
+++ tests/test_stack.c
@@ -4,20 +4,18 @@
 #include "../src/stack.h"
 #elif defined TESTS
 TEST("strlen returns length of string") {
-  Stack s = stack(3);
-
-  /*Test the size metadata is populated*/
-  ASSERT(s[1] == 3);
+  Stack s = stack(0);
 
   s = push(s, 100);
-  s = push(s, 100);
-  s = push(s, 100);
-  s = push(s, 100);
+  ASSERT(peek(s) == 100);
+  s = push(s, 200);
+  ASSERT(peek(s) == 200);
+  s = push(s, 300);
+  ASSERT(peek(s) == 300);
 
-  ASSERT(s[2] == 100);
-  ASSERT(s[3] == 100);
-  ASSERT(s[4] == 100);
-  ASSERT(s[5] == 100);
+  ASSERT(pop(s) == 300);
+  ASSERT(pop(s) == 200);
+  ASSERT(pop(s) == 100);
 }
 
 #endif
