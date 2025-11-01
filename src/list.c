57c71f3f1e61edb64bc016bfc8fac2fb6cdf3fe8
Orion Follett <orion.follett@firstresonance.io>
2025-11-01 16:54:13 -0700

    wip

--- src/list.c
+++ src/list.c
@@ -1,14 +1,35 @@
 #include "list.h"
 #include <stdlib.h>
 
+const int LEN_INDEX = 0;
+const int SIZE_INDEX = 1;
+
 List list(unsigned long size){
-    return (List)malloc(((unsigned long) 2 + size) * sizeof(int));
+    List list = (List)malloc(((unsigned long) 2 + size) * sizeof(int));
+    list[LEN_INDEX] = 0;
+    list[SIZE_INDEX] = (int)size;
+    return list;
 }
 
 void destroy_list(List list) {
     free(list);
 }
 
-List list_append(List list, int val);
+List list_append(List list, int val) {
+    int len = list[LEN_INDEX];
+    int size = list[SIZE_INDEX];
+    
+    
+    if(size <= len){
+        int new_size = size * 2;
+        list[SIZE_INDEX] = new_size;
+        list = realloc(list, ((unsigned long) 2 + new_size) * sizeof(int));
+    }
 
-int list_get(List list, int index);
+    list[len + 2] = val;
+    return list;
+}
+
+int list_get(List list, int index) {
+    return list[index + 2];
+}
