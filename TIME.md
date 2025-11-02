996fe4b03d9a3b1c0d7612c4ca3d9c4354cbfaf4
Orion Follett <orion.follett@firstresonance.io>
2025-11-02 15:59:30 -0800

    wip

--- TIME.md
+++ TIME.md
@@ -1,4 +1,4 @@
 4 hrs - project setup, stack
 3 hrs - list
 2 hr - map
-
+2 hr - asm beginnings
--- src/asm.c
+++ src/asm.c
@@ -7,23 +7,24 @@ int asm_add(int x, int y) {
   return sum;
 }
 
+// TODO inline asm wont work because gcc doesnt let you use labels which i need for loops and stuff
+// instead ill need to write in separate file and compile into it
+// thats fine because my emulator will need to do that anyways
 long asm_fib(int n) {
     /*generate the nth fibonacci number.*/
     long result;
     
-    /*
-    need:
-    - loop counter is n
-    - goto
-    - 
-    - add,
-    - 
-    */
     asm volatile(
-        "add %w0, %w1, #1"
+        "loop:\n\t"
+        "cmp %w0, #0\n\t"
+        "ble exit\n\t"
+        "add %w1, %w1, %w0\n\t"
+        "sub %w0, %w0, %w1\n\t"
+        "b loop\n\t"
+        "exit:\n\t"
         : "=r"(result) : "r"(n)
     );
-    
+    printf("RESULT %i\n", (int)result);
     return result;
 }
 
