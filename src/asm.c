c2c4cdc9cbdf52ef5982a60d61da2b6940a91e0b
Orion Follett <orion.follett@firstresonance.io>
2025-11-02 14:03:24 -0800

    incr 1

--- src/asm.c
+++ src/asm.c
@@ -3,10 +3,15 @@
 
 int asm_add(int x, int y) {
   int sum;
-
-  asm volatile("add %w0, %w1, %w2\n\t" : "=r"(sum) : "r"(x), "r"(y));
+  asm volatile("add %w0, %w1, %w2" : "=r"(sum) : "r"(x), "r"(y));
   return sum;
 }
 
-int asm_fib(int n);
+int asm_fib(int n) {
+    /*generate the nth fibonacci number.*/
+    int result;
+    asm volatile("add %w0, %w1, 1" : "=r"(result) : "r"(n));
+    return result;
+}
+
 void asm_sort(int* lis, int len);
--- tests/test_asm.c
+++ tests/test_asm.c
@@ -8,4 +8,8 @@ TEST("Test inline asm add"){
     ASSERT(asm_add(1, -2), -1);
     ASSERT(asm_add(0, 2), 2);
 }
+
+TEST("Test inline asm fib"){
+    ASSERT(asm_fib(0), 1);
+}
 #endif
