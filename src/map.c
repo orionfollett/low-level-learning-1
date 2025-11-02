790ed12a9308c9faa91dc293874566478cdb485c
Orion Follett <orion.follett@firstresonance.io>
2025-11-02 10:33:30 -0800

    wip

--- src/map.c
+++ src/map.c
@@ -1,7 +1,21 @@
 #include "map.h"
+#include <stdlib.h>
 
-Map map(unsigned long size);
-Map map_add(Map map, int key, int val);
+static int _hash(int val, int max){
+    return val % max;
+}
+
+Map map(unsigned long size) {
+    Map m;
+    m.backing_array = (MapNode*) malloc(size * sizeof(MapNode));
+    m.size = size;
+    return m;
+}
+
+Map map_add(Map map, int key, int val) {
+        
+}
+
 int map_get(Map map, int key);
 int map_pop(Map map, int key);
 void map_destroy(Map map);
--- src/map.h
+++ src/map.h
@@ -1,7 +1,20 @@
 #ifndef MAP_H
 #define MAP_H
 
-typedef int* Map;
+// Map's contain linked lists
+// So that I dont need to worry about resizing them.
+typedef struct {
+    unsigned long size;
+    MapNode* backing_array;
+} Map;
+
+// TODO: swap this to take a tagged union to support multiple types?
+// or make one that supports strings?
+typedef struct {
+    MapNode* next;
+    int key;
+    int val;
+} MapNode;
 
 Map map(unsigned long size);
 Map map_add(Map map, int key, int val);
