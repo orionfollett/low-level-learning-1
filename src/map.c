d99b98b5316303ee1ccf68297487a8a98f75ac00
Orion Follett <orion.follett@firstresonance.io>
2025-11-02 12:34:54 -0800

    wip map

--- src/map.c
+++ src/map.c
@@ -17,8 +17,38 @@ Map map_set(Map map, int key, int val) {
 
   MapNode* node = map.backing_array[index];
 
-  //
+  /*
+  traverse to the end, if we find it, change the value, if we dont find it, then
+   append to the end of the linked list
+  */
 
+  if (node == 0) {
+    MapNode* new_node = malloc(sizeof(MapNode));
+    new_node->key = key;
+    new_node->val = val;
+    new_node->next = 0;
+    map.backing_array[index] = new_node;
+    return map;
+  }
+
+  while (node != 0) {
+    if (node->key == key) {
+      node->val = val;
+      return map;
+    }
+
+    if (node->next == 0) {
+      break;
+    }
+
+    node = node->next;
+  }
+
+  MapNode* new_node = malloc(sizeof(MapNode));
+  new_node->key = key;
+  new_node->val = val;
+  new_node->next = 0;
+  node->next = new_node;
   return map;
 }
 
