61c1267c83133ed3971a2b952c7a8eaefe82ca1c
Orion Follett <orion.follett@firstresonance.io>
2025-11-02 17:28:35 -0800

    fib working

--- tests/test_asm.c
+++ tests/test_asm.c
@@ -27,6 +27,7 @@ TEST("Test add seq") {
 
 TEST("Test fib") {
   // ASSERT(asm_fib(0), 1);
+
   ASSERT(asm_fib(1), 1);
   ASSERT(asm_fib(2), 2);
   ASSERT(asm_fib(3), 3);
@@ -35,6 +36,8 @@ TEST("Test fib") {
   ASSERT(asm_fib(6), 13);
   ASSERT(asm_fib(7), 21);
   ASSERT(asm_fib(50), 20365011074);
+
+  ASSERT(asm_fib(0), 1);
 }
 
 #endif
