89b19b2a4e824122e0b82555ea2d6a95286288de
Orion Follett <orion.follett@firstresonance.io>
2025-11-04 21:21:51 -0800

    wip

--- asm/add.s
+++ asm/add.s
@@ -1,9 +1,8 @@
 .global _asm_add
 
-temp .req x0
 
 _asm_add:
     // x0 is return value
     // x0, x1, etc are args in order
-    add temp, temp, x1
+    add x0, xo, x1
     ret
--- asm/sort.s
+++ asm/sort.s
@@ -1,58 +1,56 @@
 .global _asm_sort
 
+start .req x0
+len .req x1
+last_byte .req x2
+min_index .req x3
+min .req x4
+i .req x5
+j .req x6
+curr_i .req x7
+curr_j .req x8
+temp .req x9
+
 _asm_sort:
-    // sort the list in place
-    // algorithm:
-    //def test_sort(lis):
-    //min = lis[0]
     
-    //for i in range(0, len(lis)):
-    //    min_index = i
-    //    min = lis[min_index]
-    //    for j in range(i, len(lis)):
-    //        if lis[j] < min:
-    //            min = lis[j]
-    //            min_index = j
-    //    
-    //    lis[min_index] = lis[i]
-    //    lis[i] = min
-    //    
-    //return lis
+    // convert len to bytes, multiply by 4
+    lsl len, len, #2
+    add last_byte, len, start
     
-
-    // x0 has memory address of first element in list
-    // list of ints so Ill assume 4 bytes each
-    // x1 has list length
-
-    // multiply length by 4 since we care about it in bytes
-    // say len is 3, that's actually 3 ints, which are each 4 bytes, so need
-    // to multiply by 4
-    lsl x1, x1, #2
-
-    // x1 now stores the address for the last byte in the array
-    add x1, x1, x0
-    
+    mov i, start
     outer:
-        cmp x0, x1
-        bge exit
+        cmp last_byte, i
+        ble exit
 
-        // outer loop list value
-        ldr x2, [x0]
+        ldr curr_i, [i]
+        mov min_index, i
+        mov min, curr_i
         
-        // x3 is min_value
-        mov x3, x2
-        
-        // x4 is min index
-        mov x4, x0
+        add j, i, #4
 
-        // inner loop
-        inner:
-            cmp 
-            
+        inner:    
+            cmp last_byte, j
+            ble inner_exit
+
+            ldr curr_j, [j]
+
+            cmp curr_j, min
+            bge not_min
+                // found new min
+                mov min, curr_j
+                mov min_index, j
+            not_min:
+
+            add j, j, #4
+        b inner
         inner_exit:
 
-        // increment outer loop counter
-        add x0, x0, #4
+        // insert min into beginning of outer loop
+        str min, [i]
+        str curr_i, [min_index]
+        add i, i, #4
+
+        b outer
     
     exit:
     
